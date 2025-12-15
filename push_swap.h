a485580de518d67323ccd5a8c2799deb4292b371
Antonin Do Souto <ando-sou@c2r2s10.42porto.com>
2025-12-15 19:51:24 +0000

    Initial commit: Add struct and base of main file

---
+++ push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ando-sou <ando-sou@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 19:49:33 by ando-sou          #+#    #+#             */
+/*   Updated: 2025/12/15 19:50:01 by ando-sou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_stack
+{
+	int				value;
+	int				index;
+	struct s_stack	*next;
+}	t_stack;
+
+t_stack	*parse_args(int ac, char **av);
+
+#endif
